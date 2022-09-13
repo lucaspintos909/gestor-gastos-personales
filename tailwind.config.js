40e0e9c426cab568d1970097cb41207e1768d89c
Felipe Dobrinin <fds.workmail@gmail.com>
2022-09-13 20:59:24 -0300

    adding custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-primary' : '#242424',
+        'dark-secundary' : '#363636',
+        'accent' : '#33d17a',
+        'accent-desat' : '#8ff0a4',
+        'danger' : '#e01b24',
+        'danger-desat' : '#f66151',
+        'white-self' : '#d2d2d2',
+      },
+    },
   },
   plugins: [],
 }
