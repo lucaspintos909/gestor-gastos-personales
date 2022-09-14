dbd5dd1212a2973470b7d23190f74459a963a9ee
Felipe Dobrinin <fds.workmail@gmail.com>
2022-09-13 21:52:40 -0300

    changing " to &quot;

--- pages/colors.js
+++ pages/colors.js
@@ -21,39 +21,39 @@ export default function Home() {
             <div className="grid grid-cols-2 gap-2 w-10/12">
                 <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-dark-primary rounded-lg border-2 boarder-white-self"></div>
-                    <h2 className="text-white-self font-bold  text-center whitespace-normal block">"dark-primary" : Para Fondos</h2>
+                    <h2 className="text-white-self font-bold  text-center whitespace-normal block">&quot;dark-primary&quot; : Para Fondos</h2>
                 </div>
                 <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-dark-secundary rounded-lg border-2 border-white-self"></div>
-                    <h2 className="text-white-self font-bold  text-center whitespace-normal">"dark-secundary" : Para Items</h2>
+                    <h2 className="text-white-self font-bold  text-center whitespace-normal">&quot;dark-secundary&quot; : Para Items</h2>
                 </div>
                 <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-accent rounded-lg"></div>
-                    <h2 className="text-white-self font-bold  text-center whitespace-normal">"accent" : Para focus y demas</h2>
+                    <h2 className="text-white-self font-bold  text-center whitespace-normal">&quot;accent&quot; : Para focus y demas</h2>
                 </div>
                 <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-accent-desat rounded-lg"></div>
-                    <h2 className="text-white-self font-bold  text-center whitespace-normal">"accent-desat" : Color de acento desaturado</h2>
+                    <h2 className="text-white-self font-bold  text-center whitespace-normal">&quot;accent-desat&quot; : Color de acento desaturado</h2>
                 </div>
                 <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-danger rounded-lg"></div>
-                    <h2 className="text-white-self font-bold  text-center whitespace-normal">"danger" : Color peligro </h2>
+                    <h2 className="text-white-self font-bold  text-center whitespace-normal">&quot;danger&quot; : Color peligro </h2>
                 </div>
                 <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-danger-desat rounded-lg"></div>
-                    <h2 className="text-white-self font-bold  text-center whitespace-normal">"danger-desat" : Color peligro desaturado </h2>
+                    <h2 className="text-white-self font-bold  text-center whitespace-normal">&quot;danger-desat&quot; : Color peligro desaturado </h2>
                 </div>
                 <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-white-self rounded-lg"></div>
-                    <h2 className="text-white-self font-bold  text-center whitespace-normal">"white-self" : Color peligro desaturado </h2>
+                    <h2 className="text-white-self font-bold  text-center whitespace-normal">&quot;white-self&quot; : Color peligro desaturado </h2>
                 </div>
                 <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-blue-accent rounded-lg"></div>
-                    <h2 className="text-white-self font-bold  text-center whitespace-normal">"blue-accent" : AZUL </h2>
+                    <h2 className="text-white-self font-bold  text-center whitespace-normal">&quot;blue-accent&quot; : AZUL </h2>
                 </div>
                 <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-blue-accent-desat rounded-lg"></div>
-                    <h2 className="text-white-self font-bold  text-center whitespace-normal">"blue-accent-desat" : AZUL triste :'( </h2>
+                    <h2 className="text-white-self font-bold  text-center whitespace-normal">&quot;blue-accent-desat&quot; : AZUL triste :'( </h2>
                 </div>
             </div>
         </div>
