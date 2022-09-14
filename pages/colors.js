ffce49a08277ace23a45df4458305a99595a03d9
Felipe Dobrinin <fds.workmail@gmail.com>
2022-09-13 21:35:05 -0300

    quito transicion q no anda y agrego items azules

--- pages/colors.js
+++ pages/colors.js
@@ -19,33 +19,41 @@ export default function Home() {
         <h3 className="text-xl font-semibold text-accent-desat">font : Cantarell</h3> 
         <div className="grid w-10/12 m-5">
             <div className="grid grid-cols-2 gap-2 w-10/12">
-                <div className="transition ease-in-out delay-300 flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
+                <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-dark-primary rounded-lg border-2 boarder-white-self"></div>
                     <h2 className="text-white-self font-bold  text-center whitespace-normal block">"dark-primary" : Para Fondos</h2>
                 </div>
-                <div className="transition ease-in-out delay-300 flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
+                <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-dark-secundary rounded-lg border-2 border-white-self"></div>
                     <h2 className="text-white-self font-bold  text-center whitespace-normal">"dark-secundary" : Para Items</h2>
                 </div>
-                <div className="transition ease-in-out delay-300 flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
+                <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-accent rounded-lg"></div>
                     <h2 className="text-white-self font-bold  text-center whitespace-normal">"accent" : Para focus y demas</h2>
                 </div>
-                <div className="transition ease-in-out delay-300 flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
+                <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-accent-desat rounded-lg"></div>
                     <h2 className="text-white-self font-bold  text-center whitespace-normal">"accent-desat" : Color de acento desaturado</h2>
                 </div>
-                <div className="transition ease-in-out delay-300 flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
+                <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-danger rounded-lg"></div>
                     <h2 className="text-white-self font-bold  text-center whitespace-normal">"danger" : Color peligro </h2>
                 </div>
-                <div className="transition ease-in-out delay-300 flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
+                <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-danger-desat rounded-lg"></div>
                     <h2 className="text-white-self font-bold  text-center whitespace-normal">"danger-desat" : Color peligro desaturado </h2>
                 </div>
-                <div className="transition ease-in-out delay-300 flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
+                <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
                     <div className="w-20 h-20 bg-white-self rounded-lg"></div>
                     <h2 className="text-white-self font-bold  text-center whitespace-normal">"white-self" : Color peligro desaturado </h2>
+                </div>
+                <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
+                    <div className="w-20 h-20 bg-blue-accent rounded-lg"></div>
+                    <h2 className="text-white-self font-bold  text-center whitespace-normal">"blue-accent" : AZUL </h2>
+                </div>
+                <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
+                    <div className="w-20 h-20 bg-blue-accent-desat rounded-lg"></div>
+                    <h2 className="text-white-self font-bold  text-center whitespace-normal">"blue-accent-desat" : AZUL triste :'( </h2>
                 </div>
             </div>
         </div>
