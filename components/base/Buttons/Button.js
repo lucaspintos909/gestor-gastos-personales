6c579ae2002bf504ea97dd3944cb9c6dce9873a8
Felipe Dobrinin <felipedobrinin@gmail.com>
2022-09-22 23:13:24 -0300

    added variants and colors (not working)

--- components/base/Buttons/Button.js
+++ components/base/Buttons/Button.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import {React,useState,useEffect} from 'react'
 
-export default function Button( 
-  { type = 'button', onClick, children,size='px-10 py-3'} 
-  ) {
-
+export default function Button( { type='button', onClick, children,size='px-10 py-3',variant='contained',color='success'} ) {
   
+  const [_color , setColor] = useState("");
+  const [_variant,setVariant] = useState("");
+
+  function colorHandler(){
+    let colors = {
+      'success':'accent',
+      'danger':'danger',
+      'blue':'blue-accent',
+    };
+    setColor(colors[color]);
+    console.log(_color);
+  }
+  function varaintHandler(){
+    let variants = {
+      'contained':`bg-${_color}`,
+      'outlined':`border border-${_color} text-${_color} transition-colors hover:bg-${_color}`,
+      'gradiant':`bg-gradient-to-r from-${_color} to-${_color}-desat`,
+    };
+    setVariant(variants[variant]);
+  }
+  useEffect(() => {
+    colorHandler();
+    varaintHandler();
+  },[]);
+
   return (
     <button  
       type={type} 
       onClick={onClick} 
-      className={`rounded-lg ${size} font-bold bg-gradient-to-r mt-1 from-accent to-accent-desat`}
+      className={`rounded-lg ${_variant} ${size} font-bold`}
     >
       {children}
     </button>
