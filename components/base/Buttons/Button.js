b40b1c9de0f3567abdbbe599eb989a05dfa49caf
Felipe Dobrinin <felipedobrinin@gmail.com>
2022-09-28 22:01:07 -0300

    new working button component

--- components/base/Buttons/Button.js
+++ components/base/Buttons/Button.js
@@ -1,37 +1,15 @@
 import {React,useState,useEffect} from 'react'
 
 export default function Button( { type='button', onClick, children,size='px-10 py-3',variant='contained',color='success'} ) {
-  
-  const [_color , setColor] = useState("");
-  const [_variant,setVariant] = useState("");
-
-  function colorHandler(){
-    let colors = {
-      'success':'accent',
-      'danger':'danger',
-      'blue':'blue-accent',
-    };
-    setColor(colors[color]);
-    console.log(_color);
-  }
-  function varaintHandler(){
-    let variants = {
-      'contained':`bg-${_color}`,
-      'outlined':`border border-${_color} text-${_color} transition-colors hover:bg-${_color}`,
-      'gradiant':`bg-gradient-to-r from-${_color} to-${_color}-desat`,
-    };
-    setVariant(variants[variant]);
-  }
-  useEffect(() => {
-    colorHandler();
-    varaintHandler();
-  },[]);
+  // useEffect(() => {
+    
+  // },[]);
 
   return (
     <button  
       type={type} 
       onClick={onClick} 
-      className={`rounded-lg ${_variant} ${size} font-bold`}
+      className={`rounded-lg btn-${variant} btn-${variant}-${color} ${size} font-bold`}
     >
       {children}
     </button>
