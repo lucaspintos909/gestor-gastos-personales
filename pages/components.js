2f420014fe9d1c176bddc13495897cdaf35b818f
Felipe Dobrinin <felipedobrinin@gmail.com>
2022-09-28 22:32:25 -0300

    beautyfied the page

--- pages/components.js
+++ pages/components.js
@@ -14,24 +14,42 @@ export default function Components() {
             <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <main className="bg-dark-primary">
+      <main className="bg-dark-primary flex flex-col gap-3 p-2">
         <h1 className="text-3xl font-bold text-accent">
         Componentes
         </h1>
-        <h3 className="text-xl font-semibold text-accent-desat">font : Cantarell</h3> 
-        <div className="grid grid-cols-3 gap-4 w-6/12 ">
-            <Button variant="contained" color="accent">Sexo</Button>
-            <Button variant="contained" color="danger">Sexo</Button>
-            <Button variant="contained" color="blue">Sexo</Button>
+        <h3 className="text-xl font-semibold text-accent-desat">font : Cantarell</h3>
+        <details open className="">
+          <summary><h2 className="text-2xl font-bold inline-block">Buttons</h2></summary>
+          <article className='flex flex-col gap-2 w-6/12'>
+            <h3 className="text-xl font-semibold">Types</h3>
+            <div className="grid grid-cols-3 gap-4 ">
+                <Button variant="contained" color="accent">Contained</Button>
+                <Button variant="contained" color="danger">Contained</Button>
+                <Button variant="contained" color="blue">Contained</Button>
 
-            <Button variant="outlined" color="accent">Sexo</Button>
-            <Button variant="outlined" color="danger">Sexo</Button>
-            <Button variant="outlined" color="blue">Sexo</Button>
+                <Button variant="outlined" color="accent">Outlined</Button>
+                <Button variant="outlined" color="danger">Outlined</Button>
+                <Button variant="outlined" color="blue">Outlined</Button>
 
-            <Button variant="gradiant" color="accent">Sexo</Button>
-            <Button variant="gradiant" color="danger">Sexo</Button>
-            <Button variant="gradiant" color="blue">Sexo</Button>
-        </div>
+                <Button variant="gradiant" color="accent">Gradiant</Button>
+                <Button variant="gradiant" color="danger">Gradiant</Button>
+                <Button variant="gradiant" color="blue">Gradiant</Button>
+
+                
+            </div>
+            <hr></hr>
+            <h3 className="text-xl font-semibold">Sizes</h3>
+            <div className="flex flex-row gap-2 mt">
+              <Button variant="outlined" color="accent" size="small">Small</Button>
+              <Button variant="outlined" color="danger" size="normal">Normal</Button>
+              <Button variant="outlined" color="blue" size="long">Long</Button>
+            </div>
+          </article>
+        </details>
+        
+        
+          
       </main>
 
     </div>
