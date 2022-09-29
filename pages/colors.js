c5ac980945985869ed239e2001e3a7fdbe4d7351
Felipe Dobrinin <felipedobrinin@gmail.com>
2022-09-28 22:02:15 -0300

    changed references from blue-accent to new blue color

--- pages/colors.js
+++ pages/colors.js
@@ -48,12 +48,12 @@ export default function Colors() {
                     <h2 className="text-white-self font-bold  text-center whitespace-normal">&quot;white-self&quot; : Color peligro desaturado </h2>
                 </div>
                 <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
-                    <div className="w-20 h-20 bg-blue-accent rounded-lg"></div>
-                    <h2 className="text-white-self font-bold  text-center whitespace-normal">&quot;blue-accent&quot; : AZUL </h2>
+                    <div className="w-20 h-20 bg-blue rounded-lg"></div>
+                    <h2 className="text-white-self font-bold  text-center whitespace-normal">&quot;blue&quot; : AZUL </h2>
                 </div>
                 <div className="flex flex-row gap-1 bg-dark-secundary rounded-lg p-4 outline outline-offset-1 outline-transparent hover:outline-accent">
-                    <div className="w-20 h-20 bg-blue-accent-desat rounded-lg"></div>
-                    <h2 className="text-white-self font-bold  text-center whitespace-normal">&quot;blue-accent-desat&quot; : AZUL triste :&apos( </h2>
+                    <div className="w-20 h-20 bg-blue-desat rounded-lg"></div>
+                    <h2 className="text-white-self font-bold  text-center whitespace-normal">&quot;blue-desat&quot; : AZUL triste :&apos( </h2>
                 </div>
             </div>
         </div>
